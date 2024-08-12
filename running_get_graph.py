990154484b435cf25cda3317799d4d8d456f5852
ngtm165 <ngoctamtrannguyen123@gmail.com>
2024-08-12 12:14:21 +0700

    Update running_get_graph.py

--- running_get_graph.py
+++ running_get_graph.py
@@ -12,40 +12,40 @@ warnings.filterwarnings('ignore')
 
 data=pd.read_csv('/kaggle/working/sample/data_chung/schneider50k.csv',index_col=0)
 
-def new_smi_react(smi):
-    rxnmapper = RXNMapper()
-    try:
-        mapped_smi=rxnmapper.get_attention_guided_atom_maps([smi])[0]['mapped_rxn']
-        precusor,product=smi.split('>>')
-        precusor1,product1=mapped_smi.split('>>')
+# def new_smi_react(smi):
+#     rxnmapper = RXNMapper()
+#     try:
+#         mapped_smi=rxnmapper.get_attention_guided_atom_maps([smi])[0]['mapped_rxn']
+#         precusor,product=smi.split('>>')
+#         precusor1,product1=mapped_smi.split('>>')
 
-        # Choose mapped precusor
-        ele_react=precusor.split('.')
-        ele_react1=precusor1.split('.')
-        precusor_main=[i for i in ele_react if i not in ele_react1]
-        precusor_str='.'.join(precusor_main)
-        reagent_1=[i for i in ele_react if i in ele_react1]
+#         # Choose mapped precusor
+#         ele_react=precusor.split('.')
+#         ele_react1=precusor1.split('.')
+#         precusor_main=[i for i in ele_react if i not in ele_react1]
+#         precusor_str='.'.join(precusor_main)
+#         reagent_1=[i for i in ele_react if i in ele_react1]
 
-        # Choose mapped product
-        ele_pro=product.split('.')
-        ele_pro1=product1.split('.')
-        product_main=[i for i in ele_pro if i not in ele_pro1]
-        product_main_2=[i for i in product_main if i not in precusor_main]
-        product_str='.'.join(product_main_2)
-        reagent_2=[i for i in ele_pro if i in ele_pro1]
+#         # Choose mapped product
+#         ele_pro=product.split('.')
+#         ele_pro1=product1.split('.')
+#         product_main=[i for i in ele_pro if i not in ele_pro1]
+#         product_main_2=[i for i in product_main if i not in precusor_main]
+#         product_str='.'.join(product_main_2)
+#         reagent_2=[i for i in ele_pro if i in ele_pro1]
 
-        reagent=reagent_1+reagent_2
-        reagent='.'.join(reagent)
+#         reagent=reagent_1+reagent_2
+#         reagent='.'.join(reagent)
         
-        new_react=precusor_str+'>>'+product_str
-    except:
-        new_react=np.nan
-        reagent=np.nan
+#         new_react=precusor_str+'>>'+product_str
+#     except:
+#         new_react=np.nan
+#         reagent=np.nan
 
-    return new_react,reagent
+#     return new_react,reagent
 
-data['new_rxn'],data['reagent_separated']=data['rxn'].apply(new_smi_react)
-data=data.dropna(subset=['new_rxn'])
+# data['new_rxn'],data['reagent_separated']=data['rxn'].apply(new_smi_react)
+# data=data.dropna(subset=['new_rxn'])
 
 
 
