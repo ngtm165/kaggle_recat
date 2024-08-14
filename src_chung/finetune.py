99d732d588cc62434faa85660bfb4da171b4ae53
ngtm165 <ngoctamtrannguyen123@gmail.com>
2024-08-14 10:04:59 +0700

    Update finetune.py

--- src_chung/finetune.py
+++ src_chung/finetune.py
@@ -17,7 +17,7 @@ from src_chung.util import collate_reaction_graphs
 
 def finetune(args):
 
-    batch_size = 1
+    batch_size = 2
     use_saved = False
     model_path = "/kaggle/working/sample/data_chung/model/finetuned/model.pt"
 
